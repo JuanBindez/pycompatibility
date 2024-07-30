7b5436a57ba05a6c5c12be30dec109237349a766
JuanBindez <juanbindez780@gmail.com>
2024-07-30 17:10:50 -0300

    Pycompatibility 1.0.0 Released

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'update'
-git push -u origin Release_Candidate
-git tag v1.0-rc3
+git commit -m 'Pycompatibility 1.0.0 Released'
+git push -u origin main
+git tag v1.0.0
 git push --tag
 make clean
 make upload
